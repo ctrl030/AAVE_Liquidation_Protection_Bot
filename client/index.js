5ae361526feb94ece7f3e0388267f1d7e296b035
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2021-01-20 16:28:04 -0300

    1

---
+++ client/index.js
@@ -0,0 +1,29 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>
+    <script type="text/javascript" src="assets/js/jquery-3.4.1.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+    <script src="assets/bootstrap/js/popper.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/web3.min.js"></script>
+    <script src="./abi.js"></script>
+
+    
+  </head>
+
+  <body>
+    
+    <div class="container" id="">
+      <div class="m-2 light-b-shadow" id="">
+        <h1 class=""></h1>
+        <h3 class=""></h3>
+      </div>
+    </div>  
+
+    <script src="./index.js"></script>
+   
+  </body>
+
+</html>
